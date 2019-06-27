c9219bf3f902b9581fbb3fe9e00b21bedbc7d82f
Alex <10480967+WakaToa@users.noreply.github.com>
2019-06-27 13:29:04 +0200

    Add icon

--- Browser/Main.Designer.cs
+++ Browser/Main.Designer.cs
@@ -15,17 +15,18 @@
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.SuspendLayout();
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1264, 684);
+            this.ClientSize = new System.Drawing.Size(1342, 724);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Main";
-            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DarkBot Browser";
             this.Load += new System.EventHandler(this.FrmBrowser_Load);
